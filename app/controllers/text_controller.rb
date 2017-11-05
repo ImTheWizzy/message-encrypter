1d8e03dc36d40ff82aec99da8be9ccae42696ef9
ImTheWizzy <martin_todo9@abv.bg>
2017-11-05 18:43:43 +0200

    please fam

--- app/controllers/text_controller.rb
+++ app/controllers/text_controller.rb
@@ -1,17 +1,31 @@
+require 'json'
+require 'net/http'
 class TextController < ApplicationController
-	skip_before_action :verify_authenticity_token
 $message = ""
 def index
 end
 
 def create 
 	$message = params[:text]
-	#Text.create(:text => $message)
 	render plain: "https://message-encrypter.herokuapp.com/messages"
 end
 
 def messages
 	render plain: $message
+end
+
+def api
+	message_url = "https://message-encrypter.herokuapp.com/messages"
+	
+	if request.headers["Content-Type"] == "text/xml"
+		$message = Nokogiri::XML(request.body.read).content
+		render plain: "<url>" + "#{message_url}" + "</url>" + "\n"
+	end
+	
+	if request.headers["Content-Type"] == "application/json"
+		$message = params[:message]
+		render plain: "'"'{"url":' + '"' + message_url.to_s + '"}'"'" + "\n"
+	end
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
   resources :texts
 
   root 'text#index'
-  #get '' => 'text#index'	
   post '' => 'text#create'
   get 'messages' => 'text#messages'
+  post 'messages/api' => 'text#api'
 
-  #get 'text' => 'text#index'
-  #post 'text' => 'text#create' 
-  #get 'messages' => 'text#messages'
-  #post 'messages' => 'text#messages'
 end
